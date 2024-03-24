30d9ee4b0b77edd643e6438b060b3687440d7fc0
shafiar1111 <shafiar3333@mydbs.ie>
2024-03-24 14:53:34 +0530

    url change

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
     origin:"https://travelblogfrontendreact-4.onrender.com",
+    // origin:"http://localhost:3000",
     credentials:true,
 }));
 
